a979d7bc1252e761055428fc95ccf77c5b62d0d3
USHINDI BIHAME Victoire <victoireushindi371@gmail.com>
2025-11-20 02:06:42 -0800

    Time: 6 ms (68.84%) | Memory: 57.1 MB (64.53%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,15 @@
+function smallerNumbersThanCurrent(nums) {
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                count++;
+            }
+        }
+        result.push(count);
+    }
+
+    return result;
+}
