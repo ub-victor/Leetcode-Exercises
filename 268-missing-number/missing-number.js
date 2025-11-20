29c981354cdb8efb99227206f3538e21fe445deb
USHINDI BIHAME Victoire <victoireushindi371@gmail.com>
2025-11-20 01:56:56 -0800

    Time: 1 ms (66.33%) | Memory: 55.8 MB (91.35%) - LeetSync

---
+++ 268-missing-number/missing-number.js
@@ -0,0 +1,6 @@
+function missingNumber(nums) {
+    const n = nums.length;
+    const expectedSum = (n * (n + 1)) / 2;
+    const actualSum = nums.reduce((sum, num) => sum + num, 0);
+    return expectedSum - actualSum;
+}
