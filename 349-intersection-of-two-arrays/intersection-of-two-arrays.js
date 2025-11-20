b39e22c9b224849e144ace0b285cd22208ccf209
USHINDI BIHAME Victoire <victoireushindi371@gmail.com>
2025-11-20 03:03:54 -0800

    Time: 0 ms (100.00%) | Memory: 55.4 MB (62.10%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -0,0 +1,12 @@
+ function intersection(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const result = new Set();
+
+    for (let num of nums2) {
+        if (set1.has(num)) {
+            result.add(num);
+        }
+    }
+
+    return Array.from(result);
+}
